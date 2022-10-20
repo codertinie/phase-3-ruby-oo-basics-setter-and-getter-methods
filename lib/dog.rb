2814c2faf31c205899c0e15c98119a952a32a015
codertinie <martin.munyao@moringaschool.com>
2022-10-20 16:44:35 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,29 @@
-class Dog
+# require 'pry'
 
+
+class Dog
+  attr_accessor :name
+  attr_accessor :breed
+
+  #setter methods
+  def name=(name)
+    @name = name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  #getter methods
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
 end
+# Tommy = Dog.new("Tommy", "German Shepherd")
+
+# binding.pry
+
